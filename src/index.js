78cccf7adeb219cbe8327005ead8fceb67c4adac
neret22 <ababynina@gmail.com>
2021-07-02 19:01:33 +0200

    final commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import { Provider } from "react-redux"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css';
+import {store} from "./store/store";
+import App from './components/App'
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    ,
+  document.getElementById('root')
+);
+
